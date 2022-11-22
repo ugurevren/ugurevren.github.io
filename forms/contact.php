9d6c0696a83b6997b9393e01d849f35ce021616b
Uğur Evren Çamalan <ugurevrencamalan@gmail.com>
2022-11-23 02:20:11 +0300

    .

--- forms/contact.php
+++ forms/contact.php
@@ -1,19 +1,11 @@
 <?php
-  $to = 'yourmail@yourdomain.ltd';
-  $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
-  $from = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-  $subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_SPECIAL_CHARS);
-  $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
-  
-  if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
-      $headers = ['From' => ($name?"<$name> ":'').$from,
-              'X-Mailer' => 'PHP/' . phpversion()
-             ];
-  
-      mail($to, $subject, $message."\r\n\r\nfrom: ".$_SERVER['REMOTE_ADDR'], $headers);
-      die('OK');
-      
-  } else {
-      die('Invalid address');
-  }
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = $email;
+    $to = "ugurevrencamalan@gmail.com";
+    $subject = $subject;
+    $message = $message;
+    $headers = "From:" . $name;
+    mail($to,$subject,$message, $headers);
+    echo "The email message was sent.";
 ?>
